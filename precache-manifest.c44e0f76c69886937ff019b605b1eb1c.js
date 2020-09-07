98c79d3d34ccc174059406c55ffa6a906a897365
Gozantia <lordzantiago@gmail.com>
2020-09-06 19:11:56 -0500

    Updates

---
+++ precache-manifest.c44e0f76c69886937ff019b605b1eb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07d59b412ae1fed6b12125dabf02250",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "54fe730eb9099d94ece0",
+    "url": "/drum-machine/static/css/main.e71bdfa4.chunk.css"
+  },
+  {
+    "revision": "d6f0319ae71494d37fdb",
+    "url": "/drum-machine/static/js/2.3c1b4878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.3c1b4878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fe730eb9099d94ece0",
+    "url": "/drum-machine/static/js/main.9fcdb888.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
